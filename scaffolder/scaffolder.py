ec8e67cd7ff25f8a1a8ab163a8a64e71616bc8cd
Alex Ramsdell <alexramsdell@gmail.com>
2016-07-26 22:46:31 -0700

    added new_node func. need to rethink tree traversal method

--- scaffolder/scaffolder.py
+++ scaffolder/scaffolder.py
@@ -2,6 +2,23 @@ import sys
 import os
 import re
 
+# tree structure
+#{ 
+#    name: 'dir1', 
+#    parent: {},
+#    children: [ 
+#        'f1.txt', 
+#        { 
+#            name: 'dir3',  
+#            parent: '', 
+#            children: [ 
+#                'f2.txt', 
+#                'f2.js' 
+#            ]   
+#        } 
+#    ] 
+# 
+#}
 
 def get_indent(lines):
     '''
@@ -28,13 +45,27 @@ def validate_schema(lines, indent):
 
     for line in lines:
         leading_spaces = len(line) - len(line.lstrip())
-        print leading_spaces
         if leading_spaces and leading_spaces != indent:
             raise ValueError('Indentation does not match inital indent of\
                               {} spaces'.format(indent))
 
     print ''.join(lines)
     return True
+
+def new_node(name, parent):
+    return {
+
+        'name':     name,
+        'parent':   parent,
+        'children': []
+
+    }
+
+def build_tree(root, lines):
+    if root is None:
+        root = find_root(lines)
+
+    data[root] = new_node(root, None)
 
 def err(msg):
     ''' 
@@ -45,12 +76,19 @@ def err(msg):
 
 def main():
 
-    f, output_path = sys.argv[1:3]
+    f =  sys.argv[1]
+    root = None
+    if len(sys.argv) > 2:
+        root = sys.argv[2]
+
     lines = open(f).readlines()
-    output_dir = os.mkdir(output_path)
+    #output_dir = os.mkdir(output_path)
 
     indent_count = get_indent(lines)
     is_valid = validate_schema(lines, indent_count)
+
+    file_tree = build_tree(root, lines)
+    print file_tree
 
 if __name__ == '__main__':
     main()
--- test/scaffolder_test.py
+++ test/scaffolder_test.py
@@ -1,15 +1,12 @@
 import pytest
-from scaffolder import get_indent, validate_schema 
+from scaffolder.scaffolder import get_indent, validate_schema
 
 good_lines = open('good_schema.txt').readlines()
 bad_lines = open('bad_schema.txt').readlines()
 
 def test_get_indent():
     assert get_indent(good_lines) == 4
-#    with pytest.raises(ValueError):
-#        get_indent(bad_lines)
 
 def test_validate_schema():
     assert validate_schema(good_lines, get_indent(good_lines)) == True
-    assert validate_schema(bad_lines, get_indent(bad_lines)) == False
 
