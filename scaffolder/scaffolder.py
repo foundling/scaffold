c89f4efbb1eb3d324987641ad373c718cc235d84
Alex Ramsdell <alexramsdell@gmail.com>
2016-08-01 12:34:45 -0700

    this is starting to come together conceptually

--- scaffolder/scaffolder.py
+++ scaffolder/scaffolder.py
@@ -26,6 +26,9 @@ def is_empty(line):
 def is_comment(line):
     return line.lstrip().startswith('#')
 
+def is_dir(line):
+    return line.rstrip().endswith('/')
+
 def get_indent(line):
     return len(line) - len(line.lstrip())
 
@@ -65,17 +68,46 @@ last_indent = indent_level
 for line in schema:
     indent = get_indent(line)
     filename = get_filename(line) 
-    is_dir = filename.rstrip().endswith('/')
 
     if indent > last_indent:
-        ''' We are creating a new node lower in the tree. '''
+        ''' We are creating a directory, which means a new child node of the most recent directory. '''
 
-        # get last child in current node's children 
-        node = current_node.children[-1]
+        # get node created in current node's children, becomes parent of new dir  
+        parent = current_node['children'][-1]
+
+        # if this line represents a directory, create a new node, append to parent's children
+        if is_dir(line):
+            dirname = get_dirname(line)
+            new_dir_node = new_node(parent, dirname)
+            parent['children'].append(new_dir_node)
+        else:
+            # otherwise, append its filename to parent's children
+            file_name = get_filename(line)
+            node['children'].append(file_name)
 
     if indent < last_indent:
         ''' We are creating a new node higher up or at the same level in the tree '''
-        print 'parent or sibling'
+
+        # how many units of indent is it less than current? call that N
+        # traverse N + 1 parents 
+
+        n = (last_indent - indent) / indent_size
+        num_parents = n + 1
+        target_parent = current_node
+
+        while (num_parents > 0):
+            target_parent = target_parent['parent']
+            num_parents -= 1
+
+
+
+        if is_dir(line):
+            parent = target_parent
+            dirname = get_dirname(line)
+            target_parent['children'].append(parent, dirname)
+        else:
+            filename = get_filename(line)
+            target_parent['children'].append(filename)
 
     else:
         print ''' adding children to current node '''
@@ -88,5 +120,8 @@ for line in schema:
             filename = get_filename(line)
             current_node['children'].append(filename) 
 
-    ''' update last_indent ''' 
+    # update last_indent 
     last_indent = indent
+
+    # update current_node
+    current_node = 
