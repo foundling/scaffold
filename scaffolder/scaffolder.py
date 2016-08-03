cc7d6b347f179529c78a8e15c77ac7f35f02ce5a
Alex Ramsdell <alexramsdell@gmail.com>
2016-08-03 15:08:22 -0700

    add printing callback to walk_tree. will later convert this to file creation callback

--- scaffolder/scaffolder.py
+++ scaffolder/scaffolder.py
@@ -1,8 +1,6 @@
-import pdb
-
-from utils import chomp, clean, is_empty, is_comment, is_dir, get_indent, get_filename, get_dirname, new_node,  find_ancestor, validate_schema, walk_tree
-
 ''' 
+    Scaffolder.py
+
     Generates a directory tree from a reasonable, consistently-indented flat file representation. 
 
     Rules:
@@ -15,18 +13,19 @@ from utils import chomp, clean, is_empty, is_comment, is_dir, get_indent, get_fi
 
 '''
 
-def build_tree(schema, indent_size, OUTPUT_DIR):
-    ''' use the indentation level on each line relative to the previous indentation level to build a tree structure. '''
+from utils import chomp, clean, is_empty, is_comment, is_dir, get_indent, parse_indent, get_filename, get_dirname, new_node, find_ancestor
 
-    indent = -1
+
+def build_tree(schema, indent_size, OUTPUT_DIR):
+    ''' Use indentation level of each line relative to the previous indentation level to build a tree structure. '''
 
     # virtual root provides a way to parse the indentation consistently.  
     virtual_root = new_node(parent=None, name='virtual_root', children=[])
     root = new_node(parent=virtual_root, name=OUTPUT_DIR, children=[])
     virtual_root['children'].append(root)
-
     parent_node = virtual_root
 
+    indent = -1
     for line in schema:
 
         new_indent = get_indent(line, indent_size)
@@ -38,22 +37,69 @@ def build_tree(schema, indent_size, OUTPUT_DIR):
             depth = indent - new_indent
             parent_node = find_ancestor(parent_node, depth)
 
-        if is_dir(line):
-            parent_node['children'].append( new_node(parent=parent_node, name=get_dirname(line), children=[]) )
-        else: 
-            parent_node['children'].append( new_node(parent=parent_node, name=get_filename(line), children=None) )
+        child = new_node(
+            parent   = parent_node, 
+            name     = get_dirname(line) if is_dir(line) else get_filename(line), 
+            children = [] if is_dir(line) else None
+        ) 
+        parent_node['children'].append(child)
 
         indent = new_indent
 
     return virtual_root
 
-def walk_tree(tree, indent):
+def walk_tree(tree, indent, callback):
+
     for node in tree['children']:
+        filename = node['name']
         if node['children'] is not None:
-            print ''.join([indent * ' ', node['name'] + '/'])
-            walk_tree(node, indent + 4)
+            callback(filename + '/', indent)
+            walk_tree(node, indent + 4, callback)
         else:
-            print ''.join([indent * ' ', node['name']])
+            callback(filename, indent)
+
+def validate_schema(schema_lines):
+
+    ''' Validates schema and returns indent size if valid, otherwise raises ValueError.
+
+        Once an indent size N is determined, each indentation level must be:
+
+            1) less than N by a multiple of N,  e.g. 8 -> 4 or 8 -> 0
+            2) 0, or 
+            3) preceded by a directory and greater than N by exactly N.
+    '''
+
+    def less_by_factor_of_indent(this_indent, indent):
+        ''' Returns True if the current indent reaches 0 when the global indent size is repeatedly subtracted from it. Otherwise returns False. '''  
+
+        while this_indent > 0:
+            this_indent -= indent
+
+        return this_indent == 0 
+
+    indent = None 
+    start_index = 0
+
+    # Initialize indent and line number of first indent
+    for index, line in enumerate(schema_lines):
+        if parse_indent(line) > 0:
+            indent = parse_indent(line)
+            start_index = index + 1
+            break
+
+    last_indent = indent
+    for index, line in enumerate( schema_lines[start_index:] ):
+        this_indent = parse_indent(line) 
+        difference = this_indent - last_indent
+
+        if (difference == 0) or (difference == indent) or (difference < 0 and less_by_factor_of_indent(this_indent, indent)):
+            last_indent = this_indent
+            continue
+        else:
+            raise ValueError('Parsing error on line {}:\n\n{}: {}'.format(index + start_index + 1, index + start_index +1, line))
+
+    return indent
+
 
 def main():
 
@@ -61,26 +107,18 @@ def main():
     OUTPUT_DIR = 'test_output'
 
     schema_lines = open(SCHEMA_FILE).readlines()
+    validate_schema(schema_lines)
     schema = clean(schema_lines)
 
     indent_size = validate_schema(schema)
     tree = build_tree(schema, indent_size, OUTPUT_DIR)
-    walk_tree(tree, 0)
+
+    def print_line(line, indent_size=0):
+        indented_line = indent_size * ' ' + line
+        print indented_line
+
+    walk_tree(tree, 0, callback=print_line)
 
 
 if __name__ == '__main__':
     main()
-
-'''
-
-dir1/
-app/
-    file1.txt
-    dir2/
-#    app/
-#    README.txt
-#    LICENSE.txt
-#    docs/
-#    test/
-
-'''
--- scaffolder/utils.py
+++ scaffolder/utils.py
@@ -1,3 +1,6 @@
+
+''' Utility functions for scaffolder.py '''
+
 def chomp(line):
     return line[:-1]
 
@@ -10,7 +13,12 @@ def is_comment(line):
 def is_dir(line):
     return line.rstrip().endswith('/')
 
+def parse_indent(line):
+    ''' Return the leading number of spaces in a line of text. '''
+    return len(line) - len(line.lstrip())
+
 def get_indent(line, indent_size):
+    ''' Return the number of indentation units after dividing a line of text's leading space count by some indent_size. '''
     raw_indent = len(line) - len(line.lstrip())
     return raw_indent / indent_size
 
@@ -39,105 +47,3 @@ def find_ancestor(start_node, parents_to_visit):
         parents_to_visit -= 1
 
     return current_node
-
-def validate_schema(schema):
-    ''' Takes in a list of lines and returns indentation size if valid, throws if invalid. '''
-
-    indent_size = 4
-
-    return indent_size
-
-def walk_tree(root, indent):
-    for child in root['children']:
-        if type(child) == dict:
-            print ''.join([ child['dirname'], '/' ]) 
-            walk_tree(child, indent + 4)
-        else:
-            print indent * ' ' + child
-
-def build_tree(schema, indent_size):
-
-    root = new_node(None, 'root')
-    current_node = root
-    last_indent = 0
-
-    #pdb.set_trace()
-    for line in schema:
-        indent = get_indent(line)
-        filename = get_filename(line) 
-
-        if indent > last_indent:
-            ''' We are creating a directory, which means a new child node of the most recent directory. '''
-
-            # get last node created in current node's children, becomes parent of new dir node  
-            parent = current_node['children'][-1]
-
-            # if this line represents a directory, create a new node, append to parent's children
-            # and set current node that node
-            if is_dir(line):
-                dirname = get_dirname(line)
-                new_dir_node = new_node(parent, dirname)
-                parent['children'].append(new_dir_node)
-                current_node = new_dir_node
-
-                # update last_indent because we've entered a new indentation level 
-                last_indent = indent
-
-            else:
-                # otherwise, append its filename to parent's children
-                file_name = get_filename(line)
-                parent['children'].append(file_name)
-
-
-        elif indent < last_indent:
-            ''' We are creating a new node at the level of the parent or higher in the tree '''
-
-            # how many 'units' of indent is it less than current? call that n
-            # traverse N + 1 parents 
-
-            n = (last_indent - indent) / indent_size
-            parents_to_visit = n + 1
-            target_parent = find_ancestor(current_node, parents_to_visit)
-
-            if is_dir(line):
-                parent = target_parent
-                dirname = get_dirname(line)
-                target_parent['children'].append(parent, dirname)
-            else:
-                filename = get_filename(line)
-                target_parent['children'].append(filename)
-
-            # now set the current node to be target parent
-            current_node = target_parent
-
-
-        else:
-            ''' Just adding more dirs or files to the current node's children '''
-
-            #print ''' adding children to current node '''
-            if is_dir(line):
-                parent = current_node['parent']
-                dirname = get_dirname(line)
-                new_dir_node = new_node(parent, dirname)
-                current_node['children'].append(new_dir_node)
-            else:
-                filename = get_filename(line)
-                current_node['children'].append(filename) 
-
-
-    return root
-
-def main():
-
-    schema_file = '2test.txt'
-    # remove newlines and filter out empty lines
-    schema = [  chomp(line) 
-                for line in open(schema_file).readlines() 
-                if not is_empty(line) or not is_comment(line) ]
-
-    indent_size = validate_schema(schema)
-    tree = build_tree(schema, indent_size)
-    #walk_tree(tree, 0)
-
-if __name__ == '__main__':
-    main()
