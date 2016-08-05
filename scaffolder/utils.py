99e1505092729080e4a72265e047cda85bd7ddfe
Alex Ramsdell <alexramsdell@gmail.com>
2016-08-05 16:51:56 -0700

    add tests for utils. add test files for scaffolder, tree and validator as well

--- scaffolder/utils.py
+++ scaffolder/utils.py
@@ -1,30 +1,29 @@
-'''  Utility functions for scaffolder.py.  '''
+import sys
 
-def usage():
-    print '''
-    Usage: scaffolder SCHEMA_FILE [TARGET]
-    '''
+'''  
+    Utility functions for scaffolder.py.  
+'''
+
+def usage(out=sys.stdout):
+    out.write('Usage: scaffolder SCHEMA_FILE [TARGET]')
 
 def clean(lines):
-    return [ chomp(line) for line in lines if not is_empty(line) and not is_comment(line) ] 
-
-def chomp(line):
-    return line[:-1]
+    return [ line.strip() for line in lines if not is_empty(line) and not is_comment(line) ] 
 
 def is_empty(line):
     return line.strip() is ''
 
 def is_comment(line):
-    return chomp(line).lstrip().startswith('#')
+    return line.strip().startswith('#')
 
 def is_dir(line):
     return line.rstrip().endswith('/')
 
-def is_multiple_of_indent(this_indent, indent):
+def is_multiple_of_indent(this_indent, global_indent):
     ''' Returns True if the current indent reaches 0 when the global indent size is repeatedly subtracted from it. Otherwise returns False. '''
 
     while this_indent > 0:
-        this_indent -= indent
+        this_indent -= global_indent
 
     return this_indent == 0
 
@@ -47,8 +46,12 @@ def get_indent(line, indent_size):
     return rv
             
 def get_dirname(line):
+    ''' Remove all trailing forward slashes from a line after it's been stripped. '''
+
     return line.strip().rstrip('/')
 
 def get_filename(line):
+    ''' return line with whitespace stripped. '''
+
     return line.strip()
 
