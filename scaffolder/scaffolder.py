205318c1aa7bd56332a8e5fef438d0cf4e645a3f
Alex Ramsdell <alexramsdell@gmail.com>
2016-08-08 13:41:01 -0700

    update todo. remove docs from scaffolder.py file

--- scaffolder/scaffolder.py
+++ scaffolder/scaffolder.py
@@ -13,45 +13,6 @@ import utils
 import validator
 import walk_funcs
 
-
-''' 
-    scaffolder.py
-
-    generates a directory tree from a reasonable, consistently-indented flat file representation of a file system. 
-
-    here's an example:
-
-    scaffolder/
-        scaffolder/
-            scaffolder.py
-            validator.py
-            tree.py
-            test/
-                scaffolder_test.py
-                validator_test.py
-                tree_test.py
-            readme.md
-            license.md
-        test/
-        docs/
-        readme.md
-        license.md
-
-    rules:
-        - the indentation level must be consistent throughout the schema file. 
-        - lines that end with a '/' are directories. everything else is a file. 
-        - if a command-line argument for the root directory is not given, the schema must contain a 
-        single top-level directory.
-        - if a command-line argument for the root directory is given, multiple top-level directories 
-        are allowed.
-        - blank lines (lines of length 0 after being stripped of whitespace) and comments (lines starting with 
-        '#' after
-        being stripped of whitespace) are ignored.
-        - indentation must be preceded by a directory.
-
-'''
-
-
 def main():
 
     SCHEMA_FILE, OUTPUT_DIR = utils.handle_args(sys.argv)
