3e60911fe6df7c54bbcbe1ec16fc4e7289a0117e
Alex Ramsdell <alexramsdell@gmail.com>
2016-08-10 14:24:52 -0700

    update version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "superdir",
-    version = "1.0",
+    version = "0.1.0",
     py_modules = ['superdir'],
     install_requires = [
         'superdir'
