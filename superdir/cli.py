7afb03d4621e1a173aa701f7ac208788cb616c65
Alex Ramsdell <alexramsdell@gmail.com>
2016-08-13 01:48:08 -0700

    cli and superdir slight increase in decoupling

---
+++ superdir/cli.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+'''CLI interface:
+
+superdir schema.txt [ -o my_app ]
+superdir schema.txt 
+cat schema.txt | superdir [ -o my_app ]
+superdir -h, --help
+superdir -c config
+
+'''
+
+import click
+
+@click.command(context_settings=dict(help_option_names=['-h', '--help']))
+@click.argument('schema_file', type=click.File('r'), required=True, default=sys.stdin)
+@click.option('-c','--config', nargs=1, type=str, help="Config file to read before superdir'ing your schema.")
+@click.option('-o','--outfile', nargs=1, type=str,  help="Directory name to contain your superdir'd files."
+                                                         "If none is suppied, your schema file must have exactly"
+                                                         "one top-level directory and no sibling regular files."
+                                                         "That top-level directory will be the parent of your new"
+                                                         "file tree.")
+def cli(schema_file, outfile, config):
+
+    schema = None
+
+    if schema_file is None:
+
+        # pipe
+        if not sys.stdin.isatty():
+            schema = list(sys.stdin) 
+
+    else:
+        # file
+        schema = list(schema_file)
+
+    return schema, outfile, config
--- superdir/superdir.py
+++ superdir/superdir.py
@@ -1,26 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''
 
-CLI interface:
-
-[ standard ]
-    superdir schema.txt [ -o my_app ]
-
-standard, uses schema's top-level dir (throws if there's more than one top level anything) 
-    superdir schema.txt 
-
-pipe
-    cat schema.txt | superdir [ -o my_app ]
-
-help
-    superdir -h, --help
-
-specify a config
-    superdir -c config
-
-'''
 
 from __future__ import print_function
 import datetime
@@ -32,10 +13,11 @@ import click
 from tree import Tree
 import utils
 from validator import Validator
+from cli import cli
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-def superdir(schema=None, output_dir=None, config_path=None):
+def main(schema=None, output_dir=None, config_path=None):
 
     BASE_PATH = os.path.abspath(os.curdir)
     indent_size = None
@@ -65,26 +47,6 @@ def superdir(schema=None, output_dir=None, config_path=None):
 
     directory_tree.walk(callback=create_file)
 
-@click.command(context_settings=CONTEXT_SETTINGS)
-@click.option('-o','--outfile', nargs=1, type=str,  help="Directory name to contain your superdir'd files."
-                                                         "If none is suppied, your schema file must have exactly"
-                                                         "one top-level directory and no sibling regular files."
-                                                         "That top-level directory will be the parent of your new"
-                                                         "file tree.")
-@click.option('-c','--config', nargs=1, type=str, help="Config file to read before superdir'ing your schema.")
-@click.argument('schema_file', type=click.File('r'), required=True, default=sys.stdin)
-def main(schema_file, outfile, config):
-
-    schema = None
-
-    if schema_file is None:
-        if not sys.stdin.isatty():
-            schema = list(sys.stdin) 
-    else:
-        schema = list(schema_file)
-
-    superdir(schema=schema, output_dir=outfile, config_path=config)
-
-
 if __name__ == '__main__':
-    main()
+    schema, outfile, config = cli()
+    main(schema=schema, output_dir=outfile, config_path=config)
