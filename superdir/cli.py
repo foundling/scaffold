8622000805571215c8954be9bb48c16fb07bb89c
Alex Ramsdell <alexramsdell@gmail.com>
2016-08-23 09:06:18 -0400

    more formatting

--- superdir/cli.py
+++ superdir/cli.py
@@ -17,19 +17,30 @@ import sys
 import click
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
-@click.argument('schema_file', type=click.File('r'), required=True, default=sys.stdin)
-@click.option('-c','--config', nargs=1, type=str, help="Config file to read before superdir'ing your schema.")
-@click.option('-o','--outfile', nargs=1, type=str,  help="Directory name to contain your superdir'd files."
-                                                         "If none is suppied, your schema file must have exactly"
-                                                         "one top-level directory and no sibling regular files."
-                                                         "That top-level directory will be the parent of your new"
-                                                         "file tree.")
+@click.argument(
+    'schema_file',\
+    type=click.File('r'),\
+    required=True,\
+    default=sys.stdin)
+@click.option(
+    '-c',\
+    '--config',\
+    nargs=1,\
+    type=str,\
+    help="Config file to read before superdir'ing your schema.")
+@click.option(
+    '-o',
+    '--outfile', 
+    nargs=1,
+    type=str,
+    help="Directory name to contain your superdir'd files. If none is suppied,"         "your schema file must have exactly one top-level directory and no" 
+         "sibling regular files. That top-level directory will be the parent"
+         "of your new file tree.")
 def cli(schema_file, outfile, config):
 
     schema = None
 
     if schema_file is None:
-
         # pipe
         if not sys.stdin.isatty():
             schema = list(sys.stdin) 
