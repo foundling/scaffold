11a3198e7bc4f82ae26eefbddfeb548a0d717e62
Alex Ramsdell <alexramsdell@gmail.com>
2016-08-23 11:46:07 -0400

    fix cli invocation bug (misunderstanding of click). move main into cli func

--- superdir/superdir.py
+++ superdir/superdir.py
@@ -13,6 +13,7 @@ from cli import cli
 import utils
 
 def main(schema=None, output_dir=None, config_path=None):
+    ''' '''
 
     BASE_PATH = os.path.abspath(os.curdir)
 
@@ -42,7 +43,30 @@ def main(schema=None, output_dir=None, config_path=None):
 
     directory_tree.walk(callback=create_file)
 
+
+@click.command(context_settings=dict(help_option_names=['-h', '--help']))
+@click.argument('schema_file', type=click.File('r'), required=True, default=sys.stdin)
+@click.option('-c', '--config', nargs=1, type=str, help="Config file to read before superdir'ing your schema.")
+@click.option('-o', '--outfile', nargs=1, type=str, help=("Directory name to contain your superdir'd files. If none is" 
+                                                          "supplied, your schema file must have exactly one top-level directory"         
+                                                          "and no sibling regular files. That top-level directory will be the"
+                                                          "parent of your new file tree."))
+def cli(schema_file, outfile, config):
+
+    schema = None
+
+    if schema_file is None:
+        # schema probably coming from a pipe
+        if not sys.stdin.isatty():
+            schema = list(sys.stdin) 
+
+    else:
+        # schema from a file
+        schema = list(schema_file)
+
+    main(schema=schema, output_dir=outfile, config_path=config)
+
 if __name__ == '__main__':
 
-    schema, outfile, config = cli()
-    main(schema=schema, output_dir=outfile, config_path=config)
+    cli()
+
