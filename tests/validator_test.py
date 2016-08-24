ccd07de111b6e8e8415b9b32a8a332764a9f45b7
Alex Ramsdell <alexramsdell@gmail.com>
2016-08-24 15:23:48 -0400

    rewrite validator tests

--- tests/validator_test.py
+++ tests/validator_test.py
@@ -5,41 +5,43 @@ import pytest
 
 from superdir.validator import Validator
 
-def test_validate_suceeds():
+def full_path(fname):
+    return os.path.abspath(os.path.join(os.curdir, 'tests/schemas', fname)) 
 
-    good_schema = '''
-    dir1/
-    dir2/
-        dir3/
-            dir4/
-    '''.split('\n')
-
-    validator = Validator(good_schema)
-    assert validator.validate()
-
-
-def test_validate_fails():
-
-    bad_schema = '''
-    dir1/
-    dir2/
-        dir3/
-          dir4/
-    '''.split('\n')
-
-    validator = Validator(bad_schema)
-    assert not validator.validate()
+good_schema = list(open(full_path('good_schema.txt')))
+bad_schema_1 = list(open(full_path('bad_schema1.txt')))
+bad_schema_2 = list(open(full_path('bad_schema2.txt')))
+schema_single_top_level = list(open(full_path('schema_single_top_level.txt')))
+schema_mult_top_levels = list(open(full_path('schema_mult_top_levels.txt')))
 
 def test_find_first_indent():
 
-    schema = '''
-dir1/
-dir2/
-    dir3/
-        dir4/
-    '''.split('\n')
-
-    validator = Validator(schema)
+    validator = Validator(good_schema)
     indent_data = validator._find_first_indent()
     assert indent_data['indent_size'] == 4  
-    assert indent_data['index'] == 2
+    assert indent_data['index'] == 1
+
+def test_schema_single_top_level():
+
+    top_dir_name = None
+    validator = Validator(schema_single_top_level)
+    assert validator._top_dir_is_valid(top_dir_name)
+
+def test_schema_multiple_top_levels():
+
+    top_dir_name = 'Test_Dir'
+    validator = Validator(schema_mult_top_levels)
+    assert validator._top_dir_is_valid(top_dir_name)
+
+
+    top_dir_name = None 
+    validator = Validator(schema_mult_top_levels)
+    assert not validator._top_dir_is_valid(top_dir_name)
+    
+
+def test_validate_good_schema():
+
+    validator = Validator(good_schema)
+    status = validator.validate()
+    assert status == True
+
